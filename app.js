54941517c850102ac23017061e0d8f6687bddbad
Harsh Singh <128421323+h4rshgithub@users.noreply.github.com>
2024-01-31 23:30:38 +0530

    Review

--- app.js
+++ app.js
@@ -39,7 +39,19 @@ app.use(express.static(path.join(__dirname,"/public")));
 app.get("/", (req,res) => {
     res.send("Hi , I am root");
 
-})
+});
+
+// Validastion for schema(middlewares)
+const validateListing = (req ,res , next) => {
+    let {error} = listingSchema.validate(req.body);
+    
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+              throw new ExpressError(400 , errMsg);
+    }else{
+        next();
+    }
+}
 
 //Index Route
 
@@ -65,13 +77,9 @@ app.get("/listings/:id", async  (req,res) => {
 })
 
 //Create Route
-app.post("/listings",
+app.post("/listings", validateListing,
      wrapAsync(async (req,res,next) => {
-    let result = listingSchema.validate(req.body);
-    console.log(result);
-    if(result.error){
-        throw new ExpressError(400 , result.error);
-    }
+    
         // let{title,description,image,price,country,location} = req.body;
     const newListing = new Listing (req.body.listing);
 
@@ -89,7 +97,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req,res) => {
 }));
 
 //Update Route
-app.put("/listings/:id",wrapAsync(async (req,res) => {
+app.put("/listings/:id", validateListing ,wrapAsync(async (req,res) => {
     let {id} = req.params;
      await Listing.findByIdAndUpdate(id, {...req.body.listing});
      res.redirect(`/listings/${id}`);
