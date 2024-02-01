9a97613a595f83716d171cce75342fe98b4887cf
Harsh Singh <128421323+h4rshgithub@users.noreply.github.com>
2024-02-01 12:05:39 +0530

    Third commit

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema , reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 
 
@@ -53,6 +54,17 @@ const validateListing = (req ,res , next) => {
     }
 }
 
+const validateReview = (req ,res , next) => {
+    let {error} = reviewSchema.validate(req.body);
+    
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+              throw new ExpressError(400 , errMsg);
+    }else{
+        next();
+    }
+}
+
 //Index Route
 
 app.get("/listings", async (req,res) => {
@@ -71,7 +83,7 @@ app.get("/listings/new", (req ,res) => {
 
 app.get("/listings/:id", async  (req,res) => {
     let {id} = req.params;
-     const listing = await Listing.findById(id);
+     const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs",{listing});
 
 })
@@ -110,7 +122,21 @@ app.delete("/listings/:id", wrapAsync(async (req,res ) => {
     let deletedListing =  await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-}))
+}));
+
+//Review Post Route
+app.post("/listings/:id/reviews", validateReview , wrapAsync(async(req,res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+
+}));
 
 // app.get("/testListing" , async (req,res) => {
 //     let sampleListing = new Listing ({
--- schema.js
+++ schema.js
@@ -9,5 +9,13 @@ module.exports.listingSchema = Joi.object({
         price: Joi.number().required().min(0),
         image: Joi.string().allow("",null)
     }).required()
-})
+});
+
+
+module.exports.reviewSchema = Joi.object({
+    review : Joi.object({
+        rating : Joi.number().required().min(1).max(5),
+        comment : Joi.string().required(),
+    }).required()
+});
  
