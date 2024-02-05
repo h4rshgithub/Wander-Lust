559a85c0c6c36133ff8d050a7d3fa45e19a8e54f
Harsh Singh <128421323+h4rshgithub@users.noreply.github.com>
2024-02-05 23:59:52 +0530

    initial

--- routes/review.js
+++ routes/review.js
@@ -5,17 +5,14 @@ const ExpressError = require("../utils/ExpressError.js");
 const {reviewSchema} = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview} = require("../middleware.js");
-
-
-
-
+const {validateReview, isLoggedIn} = require("../middleware.js");
 
 //Review Post Route
-router.post("/", validateReview , wrapAsync(async(req,res) => {
+router.post("/",isLoggedIn, validateReview , wrapAsync(async(req,res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
-
+    newReview.author = req.user._id;
+    console.log(newReview);
     listing.reviews.push(newReview);
 
     await newReview.save();
