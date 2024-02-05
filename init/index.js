38eb2d47be63663e835ab44035470f359e26b0d6
Harsh Singh <128421323+h4rshgithub@users.noreply.github.com>
2024-02-05 18:15:24 +0530

    initial commit

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({...obj,owner: "65bf87a39b00b6095a998f83"}))
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
--- models/listing.js
+++ models/listing.js
@@ -22,7 +22,11 @@ const listingSchema = new Schema ({
             type : Schema.Types.ObjectId,
             ref : "Review",
         }
-    ]
+    ],
+    owner : {
+        type : Schema.Types.ObjectId,
+        ref : "User",
+    }
 });
 
 listingSchema.post("findOneAndDelete", async (listing) => {
